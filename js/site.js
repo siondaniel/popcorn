01b31e50f2f874712920490ce888a548107a74d0
Sion Daniel <sirdanie@ucsc.edu>
2023-05-26 13:53:02 -0700

    Set up Color Coding for Buttons

--- js/site.js
+++ js/site.js
@@ -3,15 +3,38 @@
 // Date: 05/26/2023
 
 $(document).ready(function() {
-  var popupMessage = $('#popupMessage');
-  var infoIcon = $('#infoIcon');
-  var closeButton = $('.close-button');
+    var popupMessage = $('#popupMessage');
+    var infoIcon = $('#infoIcon');
+    var closeButton = $('.close-button');
+    var boxes = $('.box');
 
-  closeButton.on('click', function() {
-    popupMessage.hide();
-  });
+    // Hide the popup message when the close button is clicked
+    closeButton.on('click', function() {
+        popupMessage.hide();
+    });
 
-  infoIcon.on('click', function() {
-    popupMessage.show();
-  });
+    //If the popup message is visible, hide it when the user clicks anywhere else on the page
+    $(document).mouseup(function(event) {
+        if (popupMessage.is(':visible')) {
+            if (!popupMessage.is(event.target) && popupMessage.has(event.target).length === 0) {
+                popupMessage.hide();
+            }
+        }
+    });
+
+    // Show the popup message when the info icon is clicked
+    infoIcon.on('click', function() {
+        popupMessage.show();
+    });
+
+    // Toggle the clicked class when a box is clicked
+    boxes.on('click', function() {
+        $(this).toggleClass('clicked');
+
+        // Assign a unique color class to the clicked box
+        var colors = ['Adventure', 'Comedy', 'Action', 'Western', 'Documentary', 'Horror', 'Mystery'];
+        var index = $(this).index() % colors.length;
+        $(this).removeClass(colors.join(' ')).addClass(colors[index]);
+    });
+
 });

abebc7f00a55f4e2cbf76e8bf527de520ee05dbb
Sion Daniel <sirdanie@ucsc.edu>
2023-05-26 12:46:14 -0700

    Added popup info at top of page

--- js/site.js
+++ js/site.js
@@ -1,1 +1,13 @@
+// site.js - Popcorns main javascript file
+// Author: Sion Daniel
+// Date: 05/26/2023
 
+function closePopup() {
+    var popupMessage = document.getElementById('popupMessage');
+    popupMessage.style.display = 'none';
+}
+  
+window.addEventListener('DOMContentLoaded', function() {
+    var popupMessage = document.getElementById('popupMessage');
+    popupMessage.style.display = 'block';
+});

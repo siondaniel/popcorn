00b755dfcfe628db60ffa02c041da6c3fcdcf1b4
Sion Daniel <sirdanie@ucsc.edu>
2023-05-26 13:24:09 -0700

    Working Info Icon

--- js/site.js
+++ js/site.js
@@ -2,12 +2,16 @@
 // Author: Sion Daniel
 // Date: 05/26/2023
 
-function closePopup() {
-    var popupMessage = document.getElementById('popupMessage');
-    popupMessage.style.display = 'none';
-}
-  
-window.addEventListener('DOMContentLoaded', function() {
-    var popupMessage = document.getElementById('popupMessage');
-    popupMessage.style.display = 'block';
+$(document).ready(function() {
+  var popupMessage = $('#popupMessage');
+  var infoIcon = $('#infoIcon');
+  var closeButton = $('.close-button');
+
+  closeButton.on('click', function() {
+    popupMessage.hide();
+  });
+
+  infoIcon.on('click', function() {
+    popupMessage.show();
+  });
 });

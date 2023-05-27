4fdeae2d2a3652b97b9b6c74b1ca2a2b3c639ed8
Sion Daniel <sirdanie@ucsc.edu>
2023-05-26 20:42:56 -0700

    Fixed Popup height

--- js/site.js
+++ js/site.js
@@ -10,21 +10,30 @@ $(document).ready(function() {
 
     // Hide the popup message when the close button is clicked
     closeButton.on('click', function() {
+        popupMessage.removeClass('active');
+        boxes.css('margin-top', '20px');
         popupMessage.hide();
     });
 
     //If the popup message is visible, hide it when the user clicks anywhere else on the page
+    // NOT SURE WE REALLY WANT THIS
+    /*
     $(document).mouseup(function(event) {
         if (popupMessage.is(':visible')) {
             if (!popupMessage.is(event.target) && popupMessage.has(event.target).length === 0) {
+                popupMessage.removeClass('active');
+                boxes.css('margin-top', '20px');
                 popupMessage.hide();
             }
         }
     });
+    */
 
     // Show the popup message when the info icon is clicked
     infoIcon.on('click', function() {
         popupMessage.show();
+        popupMessage.addClass('active');
+        boxes.css('margin-top', popupMessage.outerHeight() + 40 + 'px');
     });
 
     // Toggle the clicked class when a box is clicked

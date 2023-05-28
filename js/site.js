2b4131c71d7959ad4f5e6bd58217740e1a939778
Sion Daniel <sirdanie@ucsc.edu>
2023-05-28 13:12:49 -0700

    Fixed sorting by genre

--- js/site.js
+++ js/site.js
@@ -43,27 +43,72 @@ var movies = {
 // Define the movie categories
 var categories = Object.keys(movies);
 
-// Shuffle the movies array (Taken from ChatGPT)
-function shuffleMovies(movieList, genre='all' ){
-    var movies = movieList.find('img');
+// Shuffle the movies array
+function shuffleMovies(movieList, categories=[] ){
+    var moviesFound = movieList.find('img');
     // Filter movies by genre
-    if (genre != 'all') {
-        movies = movies.filter('[data-category="' + genre + '"]');
+    if (categories.length > 0) {
+        moviesFound = moviesFound.filter(function() {
+            var movie = $(this);
+            var movieCategory = movie.data('category');
+            return categories.indexOf(movieCategory) !== -1;
+        });
     }
-    // Shuffle the movies
-    movies.sort(function() {
+    //    movies = movies.filter('[data-category="' + genre + '"]');
+    //}
+    // Shuffle the movies (Taken from ChatGPT)
+    moviesFound.sort(function() {
       return 0.5 - Math.random();
     });
     movieList.empty();
-    movieList.append(movies);
+    movieList.append(moviesFound);
+}
+
+// Display the movies
+function displayMovies(movieList) {
+    // Find all movies and calculate how many movies per row
+    var moviesArray = movieList.find('img');
+    var moviesPerRow = Math.ceil(moviesArray.length / 3);
+
+    // Clear the movie list
+    movieList.empty();
+
+    // Create 3 rows of movies
+    for (var i = 0; i < moviesArray.length; i += moviesPerRow) {
+        var moviesInRow = moviesArray.slice(i, i + moviesPerRow);
+        var row = $('<div class="row"></div>');
+        row.append(moviesInRow);
+        movieList.append(row);
+    }
+
+    // Display the movie list by making display: flex
+    movieList.css('display', 'flex');
+}
+
+// Create the movie list
+function createMovieList(movieList) {
+    // Create the movie list
+    for (var i = 0; i < categories.length; i++) {
+        var category = categories[i];
+        var moviesInCategory = movies[category];
+
+        for (var j = 0; j < moviesInCategory.length; j++) {
+            var movie = moviesInCategory[j];
+            // create another movie variable with the spaces removed
+            var movieNoSpaces = movie.replace(/\s/g, '');
+            var movieImage = $('<img src="img/movies/' + category + '/' + movieNoSpaces + '.png" alt="' + movie + '" data-category="' + category + '">');
+            movieList.append(movieImage);
+        }
+    }
 }
 
 $(document).ready(function() {
+    // Define variables
     var popupMessage = $('#popupMessage');
     var infoIcon = $('#infoIcon');
     var closeButton = $('.close-button');
     var boxes = $('.box');
-    var movieRows = $('.movie-list .row');
+    var selectedCategories = []; // Array to store selected categories
 
     // Hide the popup message when the close button is clicked
     closeButton.on('click', function() {
@@ -95,6 +140,7 @@ $(document).ready(function() {
 
     // Toggle the clicked class when a box is clicked
     boxes.on('click', function() {
+        // Toggle the clicked class
         $(this).toggleClass('clicked');
 
         // Assign a unique color class to the clicked box
@@ -102,18 +148,46 @@ $(document).ready(function() {
         var index = $(this).index() % colors.length;
         $(this).removeClass(colors.join(' ')).addClass(colors[index]);
 
+        // Update the selected categories array
+        var category = colors[index];
+        var isSelected = $(this).hasClass('clicked');
+
+        if (isSelected) {
+            selectedCategories.push(category);
+        } else {
+            var categoryIndex = selectedCategories.indexOf(category);
+            if (categoryIndex !== -1) {
+                selectedCategories.splice(categoryIndex, 1);
+            }
+        }
+        console.log(selectedCategories);
+
+        var movieList = $('.movie-list');
+        movieList.empty();
+        createMovieList(movieList);
+        shuffleMovies(movieList, selectedCategories);
+        displayMovies(movieList);
+
         // Define a variable for the clicked boxes
-        var categories = boxes.filter('.clicked').map(function() {
-            return $(this).text();
-        }).get();
-    
-        movieRows.each(function() {
-            var $row = $(this);
-            var $images = $row.data('originalImages');
-            var $filteredImages = $images.filter('[data-category="' + category + '"]');
+        /*var clickedBoxes = $('.clicked');
+
+        // IF there are more than 0 clicked boxes, show the movies thhat match the clicked boxes
+        // Loop through the clicked boxes
+        for (var i = 0; i < clickedBoxes.length; i++) {
+
+            // Define a variable for the clicked box
+            var clickedBox = $(clickedBoxes[i]);
             
-            $row.empty().append($filteredImages);
-        });
+            /*
+            Ex. we have this: <div class="box" id="Adventure" data-category="adventure"><p>Adventure</p></div>
+            If the user has this box selected, we want to show all movies with the data-category="adventure" attribute
+            *
+            var category = clickedBox.attr('data-category');
+            console.log(category);
+            var movieList = $('.movie-list');
+            shuffleMovies(movieList, category);
+            displayMovies(movieList);
+        }*/
     });
 
     // Define the movie list
@@ -122,42 +196,11 @@ $(document).ready(function() {
     // Clear the movie list
     movieList.empty();
 
-    // Create the movie list
-    for (var i = 0; i < categories.length; i++) {
-        var category = categories[i];
-        var moviesInCategory = movies[category];
-
-        for (var j = 0; j < moviesInCategory.length; j++) {
-            var movie = moviesInCategory[j];
-            // create another movie variable with the spaces removed
-            var movieNoSpaces = movie.replace(/\s/g, '');
-            var movieImage = $('<img src="img/movies/' + category + '/' + movieNoSpaces + '.png" alt="' + movie + '" data-category="' + category + '">');
-            movieList.append(movieImage);
-        }
-    }
-
-    // Now make the movies appear on the page
-    movieList.css('display', 'flex');
-        
-    // Create the movie list
-    // var movieList = $('.movie-list');
+    createMovieList(movieList);
+    
+    // Shuffle the movies
     shuffleMovies(movieList);
-
-    // Find all movies and calculate how many movies per row
-    var moviesArray = movieList.find('img');
-    var moviesPerRow = Math.ceil(moviesArray.length / 3);
-
-    // Clear the movie list
-    movieList.empty();
-
-    // Create 3 rows of movies
-    for (var i = 0; i < moviesArray.length; i += moviesPerRow) {
-        var moviesInRow = moviesArray.slice(i, i + moviesPerRow);
-        var row = $('<div class="row"></div>');
-        row.append(moviesInRow);
-        movieList.append(row);
-    }
-
-    // Display the movie list by making display: flex
-    movieList.css('display', 'flex');
+    
+    // Display the movies
+    displayMovies(movieList);
 });
